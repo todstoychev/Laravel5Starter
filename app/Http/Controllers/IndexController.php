210ee2a6c00a2920d7e5fb19c25f350cd7bb79cd
user <user@shtaiga>
2015-04-08 07:30:53 +0300

    Last activity record added

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ImageRequest;
-use Lib\ICR;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller {
     
